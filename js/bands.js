28a2bc9b8846f96144763a8995678ee825b3c5db
NSSMac2009EC <nssmac2009ec@NSSMac2009ECs-MacBook-Pro.local>
2018-07-12 11:33:32 -0500

    completed exercise

---
+++ js/bands.js
@@ -0,0 +1,15 @@
+let bandNumber = 0
+
+const takeNumber = function (band) {
+    bandNumber++
+    return bandNumber + ". " + band;
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
+
+const dead = takeNumber("The Dead Weather")
+console.log(dead)
